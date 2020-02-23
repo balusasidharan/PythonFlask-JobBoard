bb5ab84d87564ce31111dbeaf8a689e44e41ab7d
balusasidharan <balu.sasidharan@hotmail.com>
2020-02-23 17:12:56 -0500

    Completed module 3 Database connectivity

--- jobs/app.py
+++ jobs/app.py
@@ -42,7 +42,7 @@ def jobs():
     return render_template('index.html', jobs=jobs)
 
 
-@app.route('/jobs/<job_id>')
+@app.route('/job/<job_id>')
 def job(job_id):
     job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, '
                       'employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id where '
